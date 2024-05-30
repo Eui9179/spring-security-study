827f078e6dbbdfd144936e5d7168dfa44904565b
Eui9179 <outdark97@gmail.com>
2024-05-30 10:35:47 +0900

    UserDetailsService, UserDetails

---
+++ ssia-ch3-ex1/src/test/java/site/leui/ssiach3ex1/SsiaCh3Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package site.leui.ssiach3ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh3Ex1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
