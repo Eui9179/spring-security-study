5cf0a21a060c80f4110b1e68e7f4c44c4b575c59
Eui9179 <outdark97@gmail.com>
2024-06-05 14:54:03 +0900

    CustomAuthenticationSuccessAndFailureHandler

--- ssia-ch5-ex1/src/main/java/site/leui/ssiach5ex1/ProjectConfig.java
+++ ssia-ch5-ex1/src/main/java/site/leui/ssiach5ex1/ProjectConfig.java
@@ -1,39 +1,28 @@
 package site.leui.ssiach5ex1;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    private final CustomAuthenticationProvider customAuthenticationProvider;
+    @Autowired
+    private CustomAuthenticationSuccessHandler successHandler;
 
-    public ProjectConfig(CustomAuthenticationProvider customAuthenticationProvider) {
-        this.customAuthenticationProvider = customAuthenticationProvider;
-    }
+    @Autowired
+    private CustomAuthenticationFailureHandler failureHandler;
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(customAuthenticationProvider);
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .successHandler(successHandler)
+                .failureHandler(failureHandler)
+            .and()
+                .httpBasic()
+            .and()
+                .authorizeRequests()
+                .anyRequest().authenticated();
     }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-        userDetailsManager.createUser(User.withUsername("test").password("test").authorities("USER").build());
-        return userDetailsManager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
 }
